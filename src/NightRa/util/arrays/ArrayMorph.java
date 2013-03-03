d2cd0e4278476726534c87540132370d26867e2e
NightRa <ilan3580@gmail.com>
2013-03-03 09:35:20 +0200

    Implemented swaps.

--- src/NightRa/util/arrays/ArrayMorph.java
+++ src/NightRa/util/arrays/ArrayMorph.java
@@ -8,11 +8,11 @@ import java.util.Arrays;
 public class ArrayMorph<T> {
     private T[] array;
 
+    @SafeVarargs
     public ArrayMorph(T... array) {
         this.array = array;
     }
 
-    //Make it a wrapper class maybe?
     public T[] shift(int shift) {
         int length = array.length;
         IntBase base = new IntBase(length);
@@ -20,6 +20,7 @@ public class ArrayMorph<T> {
         for (int i = 0; i < length; i++) {
             shifted[base.Mod(i + shift)] = array[i];
         }
+        setArray(shifted);
         return shifted;
     }
 
@@ -31,7 +32,33 @@ public class ArrayMorph<T> {
         return shift(1);
     }
 
+    public T[] swap(int index1, int index2) {
+        T temp = array[index1];
+        array[index1] = array[index2];
+        array[index2] = temp;
+        return array;
+    }
+
     public T[] getArray() {
+        return array;
+    }
+
+    public void setArray(T[] array) {
+        this.array = array;
+    }
+
+    public T[] swapPairs() {
+        int length = array.length;
+        for (int i = 0; i < (length % 2 == 0 ? length : length - 1); i += 2) {
+            swap(i, i + 1);
+        }
+        return array;
+    }
+
+    public T[] backwards() {
+        for (int i = 0; i < array.length / 2; i++) {
+            swap(i,array.length-i);
+        }
         return array;
     }
 }
--- test/NightRa/util/arrays/ArrayMorphTest.java
+++ test/NightRa/util/arrays/ArrayMorphTest.java
@@ -7,22 +7,58 @@ import org.junit.Test;
 public class ArrayMorphTest {
     @Test
     public void testShift() throws Exception {
-        ArrayMorph<Integer> array = new ArrayMorph<Integer>(5,3,8,9,2);
+        ArrayMorph<Integer> array = new ArrayMorph<>(5,3,8,9,2);
         Integer[] expected = {8,9,2,5,3};
         Assert.assertArrayEquals(expected,array.shift(-2));
     }
 
     @Test
     public void testShiftLeft() throws Exception {
-        ArrayMorph<Character> array = new ArrayMorph<Character>('a','b','c','d','e');
+        ArrayMorph<Character> array = new ArrayMorph<>('a','b','c','d','e');
         Character[] expected = {'b','c','d','e','a'};
-        Assert.assertArrayEquals(expected,array.shiftLeft());
+        array.shiftLeft();
+        Assert.assertArrayEquals(expected,array.getArray());
     }
 
     @Test
     public void testShiftRight() throws Exception {
-        ArrayMorph<String> array = new ArrayMorph<String>("Hello","World","How","Are","You?");
+        ArrayMorph<String> array = new ArrayMorph<>("Hello","World","How","Are","You?");
         String[] expected = {"You?","Hello","World","How","Are"};
         Assert.assertArrayEquals(expected,array.shiftRight());
+    }
+
+    @Test
+    public void testSwap() throws Exception {
+        ArrayMorph<Integer> array = new ArrayMorph<>(5,3,8,9,5);
+        array.swap(1,3);
+        Integer[] expected = {5,9,8,3,5};
+        Assert.assertArrayEquals(expected,array.getArray());
+    }
+
+    @Test
+    public void testSwapPairsOdd() throws Exception {
+        ArrayMorph<Integer> array = new ArrayMorph<>(5,3,8,9,5);
+        array.swapPairs();
+
+        Integer[] expected = {3,5,9,8,5};
+        Assert.assertArrayEquals(expected,array.getArray());
+    }
+
+    @Test
+    public void testSwapPairsEven() throws Exception {
+        ArrayMorph<Integer> array = new ArrayMorph<>(13,5,36,13);
+        array.swapPairs();
+
+        Integer[] expected = {5,13,13,36};
+        Assert.assertArrayEquals(expected,array.getArray());
+    }
+
+    @Test
+    public void testBackwards() throws Exception {
+        ArrayMorph<Integer> array = new ArrayMorph<>(1,2,3,4,5,6);
+        array.backwards();
+
+        Integer[] expected = {6,5,4,3,2,1};
+        Assert.assertArrayEquals(expected,array.getArray());
     }
 }
